21e7f1f84992e3c21d90f36edeb9d159a5169cd0
Alexander Moya Hin <Alexander.hin@student.umn.ac.id>
2020-07-19 22:23:13 +0700

    Home Page Edit
    -Welcome to bios belom
    -dikiri ada blankspace

--- application/views/page/Home.php
+++ application/views/page/Home.php
@@ -37,78 +37,83 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 
 
-    <section class="ftco-intro py-5" style="background-image: url(assets/resources/home/bg-home2.png);" id="about-section">
+    <section class="ftco-intro py-5" style="background-color:#ab206c" id="about-section">
         <div class="overlay"></div>
         <div class="container">
             <div class="row d-flex align-items-center">
                 <div class="col-md-6 pl-md-5 ftco-animate" style="padding: 20px">
                     <h2 style="border-bottom: 5px solid white;"><a href="#" style="font-weight:bold">About US</a></h2>
-                    <p style="color: white">BIOS (Building Informatics’ Competitive Soul) merupakan rangkaian acara yang terdiri dari seminar dan kompetisi Hackathon yang diselenggarakan oleh Himpunan Mahasiswa Informatika (HMIF) Universitas Multimedia Nusantara. Tema dari BIOS pada tahun 2019 yaitu "Bring Innovation Into Solution" yang mencerminkan semangat mahasiswa yang kreatif dalam membawa inovasi menjadi solusi.</p>
-                    <h2 style="border-bottom: 5px solid white; text-align: right"><a href="#" style="font-weight:bold;">Organized By</a></h2>
-                    <div style="text-align: right">
-                        <img class="col-5" src="<?php echo base_url(); ?>/assets/resources/home/logo-hmif.png" style="background-color: white; border-radius: 50px; padding: 10px; padding-left: 30px;  padding-right: 30px; margin-top: 20px">
+                    <p style="color: white">BIOS (Building Informatic's Competitive Soul) merupakan rangkaian acara yang terdiri dari seminar dan kompetisi Hackaton yang dislenggarakan oleh Himpunan Mahasiswa Informatika (HMIF) Universitas Multimedia Nusantara.
+                        <br><br>
+                        Tema : Fill the Future with Creativity</p>
+                    <h2><a href="#" style="font-weight:bold;">Organized By</a></h2>
+                    <hr style="margin:0px;width:30%;border-color:#ffffff;background-color:#ffffff">
+                    <div style="text-align: left">
+                        <img class="col-5" src="<?php echo base_url('/assets/resources/home/logo-hmif.png'); ?>" style="background-color: white; border-radius: 50px; padding: 10px; padding-left: 30px;  padding-right: 30px; margin-top: 20px">
                     </div>
                 </div>
                 <div class="col-md-6 ftco-animate">
-                    <img class="col-12 col-sm-12" src="<?php echo base_url(); ?>/assets/resources/home/pict-about.png" style=" text-align: right">
+                    <img class="col-12 col-sm-12" src="<?php echo base_url('/assets/img/aboutus-img.png'); ?>" style=" text-align: right">
                 </div>
             </div>
             <div class="row d-flex align-items-center" style="margin-top: 60px">
+                <div class="col-md-6 ftco-animate">
+                    <img class="col-12 col-sm-12" src="<?php echo base_url('assets/img/seminar-img.png'); ?>">
+                </div>
                 <div class="col-md-6 pl-md-5 ftco-animate" style="padding: 20px">
                     <h2 style="border-bottom: 5px solid white;"><a href="#" style="font-weight:bold">Seminar</a></h2>
-                    <p style="color: white">Seminar yang membahas mengenai penerapan Artificial Intelligence dan Augmented Reality pada teknologi MBUX oleh Mercedes-Benz.</p>
-                    <p style="color: white"><a href="<?php echo base_url() ?>Seminar" class="btn btn-primary px-4 py-2" style="text-transform: uppercase; font-size: 12px">Read More</a></p>
-                </div>
-                <div class="col-md-6 ftco-animate">
-                    <img class="col-12 col-sm-12" src="<?php echo base_url(); ?>/assets/resources/home/pict-seminar.png">
+                    <p style="color: white">Seminar yang membahas mengenai penerapan Artificial Intelligence dan Augmented Reality pada teknologi MBUX oleh Mercedes-Benz Distribution Indonesia</p>
+                    <p style="color: white"><a href="<?php echo base_url() ?>Seminar" class="px-4 py-2" style="color:#ffffff;background-color:#301b40;text-transform: uppercase; font-size: 12px">Read More ></a></p>
                 </div>
             </div>
             <div class="row d-flex align-items-center" style="padding: 20px; ">
-                <div class="col-md-6 pl-md-5 ftco-animate">
-                    <img class="col-12 col-sm-12" src="<?php echo base_url(); ?>/assets/resources/home/pict-hackathon.png">
-                </div>
-                <div class="col-md-6 ftco-animate" style="text-align: right">
+                <div class="col-md-6 ftco-animate" style="text-align: left">
                     <h2 style="border-bottom: 5px solid white;"><a href="#" style="font-weight:bold">Hackathon</a></h2>
                     <p style="color: white">Perlombaan yang menantang peserta untuk memecahkan problem yang diberikan menjadi sebuah solusi berupa prototipe aplikasi yang inovatif selama 24 jam.</p>
-                    <p><a href="<?php echo base_url() ?>Hackathon" class="btn btn-primary px-4 py-2" style="text-transform: uppercase; font-size: 12px">Read More</a></p>
+                    <p style="color: white"><a href="<?php echo base_url() ?>Hackaton" class="px-4 py-2" style="color:#ffffff;background-color:#301b40;text-transform: uppercase; font-size: 12px">Read More ></a></p>
+                </div>
+                <div class="col-md-6 pl-md-5 ftco-animate">
+                    <img class="col-12 col-sm-12" src="<?php echo base_url('/assets/img/hackathon-img.png'); ?>">
                 </div>
             </div>
             <div class="row d-flex align-items-center" style="padding: 20px; ">
-                <div class="col-md-6 pl-md-5 ftco-animate">
-                    <img class="col-12 col-sm-12" src="<?php echo base_url(); ?>/assets/resources/home/pict-sponsor.png">
+                <div class="col-md-2 ftco-animate">
+                    <img class="col-12 col-sm-12" src="<?php echo base_url('assets/img/shadow.png'); ?>">
                 </div>
-                <div class="col-md-6 ftco-animate">
-                    <h2 style="border-bottom: 5px solid white;"><a href="#" style="font-weight:bold">Sponsors</a></h2>
-                    <div class="col-md-12 d-flex" style="background-color: white; border-radius: 25px; padding: 10px;">
+                <div class="col-md-8 ftco-animate">
+                    <h2 style="border-bottom: 5px solid white; text-align:center;"><a href="#" style="font-weight:bold">Sponsors</a></h2>
+                    <div class="col-md-12 d-flex" style=" border-radius: 25px; padding: 10px;">
                         <div class="col-md-3" style="float: left">
                             <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
                         </div>
-                        <div class="col-md-4" style="float: left">
-                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-2.png" class="col-12" style="margin: 0px; padding: 0px">
-                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-3.png" class="col-12" style="margin: 0px; padding: 0px">
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
                         </div>
-                        <div class="col-md-4" style="float: left">
-                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-4.jpg" class="col-12" style="margin: 0px; padding: 0px">
-                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-5.png" class="col-12" style="margin: 0px; padding: 0px">
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
                         </div>
-                    </div>
-                    <h2 style="border-bottom: 5px solid white; margin-top: 20px"><a href="#" style="font-weight:bold">Media Partners</a></h2>
-                    <div class="col-md-12 d-flex" style="background-color: white; border-radius: 25px; padding: 10px;">
-                        <div class="col-md-4" style="float: left; padding-top: 0px">
-                            <img src="http://localhost/Bios//assets/resources/medpar/sponsor-1.png" class="col-12" style="margin-top: 10px; padding: 0px">
-                            <img src="http://localhost/Bios//assets/resources/medpar/sponsor-2.png" class="col-12" style="margin: 0px; padding: 0px">
-                            <img src="http://localhost/Bios//assets/resources/medpar/sponsor-3.png" class="col-12" style="margin-top: 10px; margin-bottom: 10px; padding: 0px">
-                        </div>
-                        <div class="col-md-4" style="float: left">
-                            <img src="<?php echo base_url(); ?>/assets/resources/medpar/sponsor-4.png" class="col-12" style="margin: 0px; padding: 0px">
-                            <img src="<?php echo base_url(); ?>/assets/resources/medpar/sponsor-5.png" class="col-12" style="margin: 0px; padding: 0pxx">
-                            <img src="<?php echo base_url(); ?>/assets/resources/medpar/sponsor-6.png" class="col-12" style="margin: 0px; padding: 0px">
-                        </div>
-                        <div class="col-md-4" style="float: left">
-                            <img src="<?php echo base_url(); ?>/assets/resources/medpar/sponsor-7.png" class="col-10" style="margin: 0px; padding: 10px">
-                            <img src="<?php echo base_url(); ?>/assets/resources/medpar/sponsor-8.png" class="col-10" style="margin: 0px; padding: 0px">
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
                         </div>
                     </div>
+                    <h2 style="border-bottom: 5px solid white; margin-top: 20px;text-align:center;"><a href="#" style="font-weight:bold">Media Partners</a></h2>
+                    <div class="col-md-12 d-flex" style=" border-radius: 25px; padding: 10px;">
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
+                        </div>
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
+                        </div>
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
+                        </div>
+                        <div class="col-md-3" style="float: left">
+                            <img src="<?php echo base_url(); ?>/assets/resources/sponsor/sponsor-1.png" class="col-12" style="margin: 0px; padding: 0px">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-2 ftco-animate">
+                    <img class="col-12 col-sm-12" src="<?php echo base_url('assets/img/shadow.png'); ?>">
                 </div>
             </div>
         </div>
